81033b5a7a6e08a3de0c3ea1c0c906960769b75e
brbrov <epoc@list.ru>
2022-10-11 22:23:15 +0300

    feat: Add dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let codeName = [];
+  if(!Array.isArray(members) || members.length === 0){
+    return false;
+  }
+  for (let word of members) {
+    if (typeof word === 'string' && word.search(/[0-9]/g) === -1) {
+    word = word.trimStart().toUpperCase();
+    codeName.push(word.slice(0,1));       
+    }
+  }
+  codeName.sort();
+  return codeName.join('');
 }
 
 module.exports = {
