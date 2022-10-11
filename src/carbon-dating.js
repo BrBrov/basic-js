0ec75046b25f5c8c12efee111c6696296e812394
brbrov <epoc@list.ru>
2022-10-11 18:12:23 +0300

    feat: Add count cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,23 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let value = undefined;
+  if(typeof sampleActivity === 'string'){
+    let value = parseFloat(sampleActivity);
+  }else{
+    return false;
+  }  
+  if(!value || value < 0 || value > 9999){
+    return false;
+  }
+  let k = Math.LN2/HALF_LIFE_PERIOD;
+  let kt = Math.log2(MODERN_ACTIVITY/sampleActivity);
+  let age = Math.ceil((kt/k));
+  return age;
+
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,4 @@
 const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
@@ -14,9 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = 0;
+  if(matrix.length === 0){
+    return count;
+  }
+  matrix = matrix.flat(1);
+  matrix.find(e=>{
+    if(e === '^^'){
+      count++;
+    }
+  })
+  return count;
 }
 
 module.exports = {
