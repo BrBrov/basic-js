0151d19e97b677e1ea34af65cd4b332a28eecf62
brbrov <epoc@list.ru>
2022-10-14 19:47:47 +0300

    feat: create extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+ function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!str || typeof str !== 'string') {
+    str = `${str}`;
+  }
+  if(options.hasOwnProperty(`addition`)){
+    let addition = options.addition
+    options.addition = `${addition}`;
+  }
+  if (!options.hasOwnProperty('separator')) {
+    options.separator = `+`;
+  }
+  if (!options.additionSeparator) {
+    options.additionSeparator = `|`;
+  }
+  let StringAddition = '';
+  if(options.hasOwnProperty(`addition`)) {
+    let preStringAddition = `${options.addition}${options.additionSeparator}`;
+    if (options.additionRepeatTimes) {
+      for (let i = 0; i < options.additionRepeatTimes - 1; i++) {
+        StringAddition += preStringAddition;
+      }
+      StringAddition += `${options.addition}`;
+    }else{
+      StringAddition = `${options.addition}`;
+    }
+  }
+  let result = '';
+  let preResult = '' 
+  if(options.hasOwnProperty(`repeatTimes`)) {
+    preResult = `${str}${StringAddition}${options.separator}`;
+    for (let i = 0; i < options.repeatTimes - 1; i++) {
+      result += preResult;
+    }
+    result += `${str}${StringAddition}`;
+  }else{
+    result = `${str}` + StringAddition;
+  }
+  return result;
 }
 
 module.exports = {
