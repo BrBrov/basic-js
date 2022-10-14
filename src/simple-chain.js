bb2c8ba3f4e1830fa6e75e5aa277a9918090eb7d
brbrov <epoc@list.ru>
2022-10-14 13:15:35 +0300

    feat: create simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+ const chainMaker = {
   getLength() {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    if(this.hasOwnProperty('links')){
+    if (this.hasOwnProperty('links')) {
       return this.links.length;
-    }else{
+    } else {
       return 0;
-    }    
+    }
   },
   addLink(value) {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
     // chainable
-    if(!value){
-      value = '( )';
-    }  
-    if(this.hasOwnProperty('links')){
+    if (!value) {
+      switch (value) {
+        case null:
+          value = null;
+          break;
+        case 0:
+          value = '0';
+          break;
+        case false:
+          value = false;
+          break;
+        case true:
+          value = true;
+          break;
+        case Infinity:
+          value = Infinity;
+          break;
+        default:
+          if (isNaN(value)) {
+            value = NaN;
+          } else {
+            value = '( )';
+          }
+          break;
+      }
+    }
+    if (this.hasOwnProperty('links')) {
       this.links.push(`( ${value} )`);
-    }else{
+    } else {
       this.links = [];
       this.links.push(`( ${value} )`);
     }
@@ -33,8 +56,11 @@ const chainMaker = {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
     // chainable
-    if(this.getLength() < position){
-      throw new Error('You can\'t remove incorrect link!');
+    if (typeof position === 'string' || position < 1 || position > this.getLength() - 1 || !Number.isInteger(position)) {
+      delete this.links;
+      let err = new Error('You can\'t remove incorrect link!');
+      err.name = 'Fucking BasicJS';
+      throw err;
     }
     this.links.splice((position - 1), 1);
     return this;
@@ -43,13 +69,23 @@ const chainMaker = {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
     // chainable
-    this.links.reverse();
-    return this;    
+    if (this.hasOwnProperty('links')) {
+      this.links.reverse();
+    } else {
+      this.links = [];
+    }
+    return this;
   },
   finishChain() {
     // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    return this.links.join('~~');
+    if (this.hasOwnProperty('links')) {
+      let result = this.links.join('~~');
+      delete this.links;
+      return result;
+    }else{
+      delete this.links;
+    }
   }
 };
 
