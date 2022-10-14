0fdf0c647c513dbd29dc371189f71c9565102e3f
brbrov <epoc@list.ru>
2022-10-14 23:45:28 +0300

    feat: create common character count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+ function getCommonCharacterCount(s1, s2) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  s1 = [...s1];
+  s2 = [...s2];
+  let count1 = {};
+  let count2 = {};
+  s1.forEach(e =>{
+    if(count1.hasOwnProperty(`${e}`)){
+      count1[`${e}`]++;
+    }else{
+      count1[`${e}`] = 1;
+    }
+  })
+  s2.forEach(e =>{
+    if(count2.hasOwnProperty(`${e}`)){
+      count2[`${e}`]++;
+    }else{
+      count2[`${e}`] = 1;
+    }      
+  }) 
+  let summery = 0;
+  for(const key in count1){
+      if(count2.hasOwnProperty(key)){
+          summery += (count2[key] > count1[key]) ? (count1[key]) : (count2[key]);
+      }
+  }
+  return summery;
 }
 
 module.exports = {
