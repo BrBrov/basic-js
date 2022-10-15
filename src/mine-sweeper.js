24314093b0f5d22e0c0a98f8ba5e16ff9c2f81e4
brbrov <epoc@list.ru>
2022-10-15 15:09:39 +0300

    feat: create minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+ function minesweeper(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = [];
+  matrix.forEach((row, index, arr)=>{
+      let saver = [];
+      row.forEach((elem, i)=>{
+          let count = 0;
+          //up
+          if((i - 1) >=0 && (index - 1) >=0 && arr[index - 1][i - 1] === true){
+              count++;
+          }
+          if((index - 1) >=0 && arr[index - 1][i] === true){
+              count++;
+          }
+          if((index - 1) >=0 && arr[index - 1][i + 1] === true){
+              count++;
+          }
+          // middle
+          if((i - 1) >=0 && arr[index][i - 1] === true){
+              count++;
+          }
+          if((i + 1) < arr[index].length && arr[index][i + 1] === true){
+              count++;
+          }
+          // down
+          if((index + 1) < arr.length && (i - 1) >= 0 && arr[index + 1][i - 1] === true){
+              count++;
+          }
+          if((index + 1)< arr.length && arr[index + 1][i] === true){
+              count++;
+          }
+          if((index + 1)< arr.length && (i + 1)<arr[index].length && arr[index + 1][i + 1] === true){
+              count++;
+          }
+          //default
+          saver.push(count);
+      }, arr);
+      result.push(saver);
+  });    
+  return result;
 }
 
 module.exports = {
