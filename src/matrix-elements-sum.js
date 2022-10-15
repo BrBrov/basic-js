7a887914d9413bb31b0c69562765c096971ac924
brbrov <epoc@list.ru>
2022-10-15 15:25:01 +0300

    feat: create matrix elements count

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+ function getMatrixElementsSum(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = matrix[0].reduce((a,b)=>{return a+b})
+  for(let i = 1; i < matrix.length; i++){
+      matrix[i].forEach((element, index) => {
+          if(matrix[i - 1][index] !== 0){
+             count += element; 
+          }
+      });
+  }
+  return count;
 }
 
 module.exports = {
