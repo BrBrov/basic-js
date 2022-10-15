331443c9ed271c550a4379cff15ea8aad4267a9d
brbrov <epoc@list.ru>
2022-10-15 10:52:14 +0300

    feat: create encode lines

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+ function encodeLine(str) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = [];
+  let count = 0;
+  let elem = '';
+  [...str].forEach((e, i, arr) => {
+      if(arr[i + 1] !== e){            
+          if(count !== 0){
+              count++;
+              result.push(`${count}${e}`);
+              count = 0;
+          }else{
+              result.push(e);
+          }
+          
+      }else{
+          count++;
+      }
+  });
+  return result.join('');
 }
 
 module.exports = {
